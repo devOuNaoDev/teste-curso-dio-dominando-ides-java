81ae9544ada06e8b9e5e2f7e04efe71ffd223259
devOuNaoDev <94649047+devOuNaoDev@users.noreply.github.com>
2022-03-19 14:58:58 -0300

    Add files via upload
    arquivos adicionados

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.exemplo.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato = new Gato (null, null, null);
+		
+		System.out.println(gato);
+		
+
+	}
+
+}
